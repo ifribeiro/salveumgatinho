75297d2dcfa081103527532aeff247e41b287bb2
Iran Ribeiro <ifreitasribeiro@hotmail.com>
2018-10-19 16:46:14 -0300

    Remoção do teste de conexão com o banco

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,7 +4,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 
 	public function index()
-	{
+	{	
 		$this->load->view('home');
+	}
+
+	public function testarbanco(){
+		/*
+		$this->load->database();
+		$query = $this->db->query('SELECT * FROM teste');
+		foreach ($query->result() as $row){
+        	echo $row->id;
+        	echo $row->nome;
+		}*/
 	}
 }
