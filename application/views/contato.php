21b500fe4eb5e3a2a0ed6ff5be1c6398e7d26b4b
Iran Ribeiro <ifreitasribeiro@hotmail.com>
2018-11-16 10:17:44 -0300

    - Adição do icone que aparece na aba do navegador - Adição da logo na navbar - Criação da página de contatos - Criação da página de informações - Adição de alguns estilos

--- application/views/contato.php
+++ application/views/contato.php
@@ -1,1 +1,37 @@
-<h1>Contato salve um gatinho!</h1>
+<div class="container">
+    <div class="container-principal">
+        <header class="text-center">
+            <h2>Entre em contato com a gente!</h2>            
+        </header>
+        <div id="campos-contato" class="row">     
+            <div class="col-lg-3 offset-lg-1 rounded">
+                <form>
+                    <header class="text-center">E-mail</header>
+                    <div class="form-group">
+                        <label for="nome-contato">Nome</label>
+                        <input size="10" type="text" class="form-control" id="nome-contato" placeholder="Insira seu nome">                    
+                    </div>
+                    <div class="form-group">
+                        <label for="email-contato">Email</label>
+                        <input type="email" class="form-control" id="email-contato" placeholder="Seu e-mail">
+                    </div>
+                    <div class="form-group">
+                        <label for="mensagem-contato">Mensagem</label>
+                        <textarea rows="" cols="26"></textarea>
+                    </div>           
+                    <button type="button" class="btn btn-primary">Enviar</button>
+                </form>
+            </div>            
+            <div class="col-lg-3 offset-lg-3">
+            <header class="text-center"> Redes Sociais </header>
+            <div class="fb-page" 
+                data-href="https://www.facebook.com/salveumgatinhovca"
+                data-width="380" 
+                data-hide-cover="false"
+                data-show-facepile="false"></div>
+            </div>            
+        </div>
+    </div>   
+</div>
+<!-- .container!-->
+  
--- application/views/informacoes.php
+++ application/views/informacoes.php
@@ -1,1 +1,80 @@
-<h1>Informacoes salve um gatinho!</h1>
+<div class="container">
+    <div class="container-principal">
+        <header class="text-center">
+            <h2>Informações</h2>            
+        </header>
+        <div id="cards-informacoes" class="row text-center">
+        <!-- INICIO DO CARD !-->
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+            <div class="card-header card-informacoes-titulo">Resgate</div>
+            <div class="card-body">              
+              <p class="card-text">Encontrei um gatinho que precisa de ajuda, e agora?</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Saiba mais!</a>
+            </div>
+          </div>
+        </div>
+        <!-- FIM DO CARD !-->
+
+        <!-- INICIO DO CARD !-->
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+          <div class="card-header card-informacoes-titulo">Adoção</div>
+            <div class="card-body">     
+              <p class="card-text">Quero adotar um gatinho, o que preciso?</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Saiba mais!</a>
+            </div>
+          </div>
+        </div>     
+        <!-- FIM DO CARD !-->
+
+        <!-- INICIO DO CARD !-->
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+            <div class="card-header card-informacoes-titulo">Castração</div>
+            <div class="card-body">              
+              <p class="card-text">Por que você deve castrar seu gatinho?</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Saiba mais!</a>
+            </div>
+          </div>
+        </div>
+        <!-- FIM DO CARD !-->
+
+        <!-- INICIO DO CARD !-->
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+          <div class="card-header card-informacoes-titulo">Quero Ajudar</div>
+            <div class="card-body">             
+              <p class="card-text">Tenho interesse em ajudar a ONG, o que posso fazer?</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Saiba mais!</a>
+            </div>
+          </div>
+        </div>
+        <!-- FIM DO CARD !-->
+
+        <!-- INICIO DO CARD !-->
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+          <div class="card-header card-informacoes-titulo">Lar temporário</div>
+            <div class="card-body">
+              <p class="card-text">Como funciona o Lar Temporário?</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Saiba mais!</a>
+            </div>
+          </div>
+        </div>
+        <!-- FIM DO CARD !-->
+
+      </div>
+      <!-- /.row -->
+    </div>   
+</div>
