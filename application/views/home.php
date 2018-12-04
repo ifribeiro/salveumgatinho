f63ae70dbf42b0ceaf34927c84fe5fccf3dddc6b
jonascesar95 <jonascesar95@gmail.com>
2018-12-04 17:57:03 -0300

    Alterações Home

--- application/views/home.php
+++ application/views/home.php
@@ -1,16 +1,43 @@
 
 
     <!-- Page Content -->
-    <div class="container">
+    <div class="container mb-4 mt-5 clearfix">
 
-      <!-- Jumbotron Header -->
+      <!-- 3 BOTÕES -->
+      <div class="col-lg-4 float-left">
+
+        <div class="m-4 botaoRedondo border bg-info border-success rounded-circle">
+          <a href="">Quero adotar</a>
+        </div>
+
+        <div class="m-4 botaoRedondo border bg-info border-success rounded-circle">
+          <a href="">Quero adotar</a>
+        </div>
+
+        <div class="m-4 botaoRedondo border bg-info border-success rounded-circle">
+          <a href="">Quero adotar</a>
+        </div>
+
+      </div>
+
+      <!-- Texto -->
+
+      <div class="texto-apresentacao  col-lg-8 float-right">
+        <p>
+        Use os utilitários de borda para estilizar rapidamente a borda e o raio da borda de um elemento. Ótimo para imagens, botões ou qualquer outro elemento.
+        </p>
+      
+      </div>
+      
+      </div>
+      <!--
       <header class="jumbotron my-4">
         <h1 class="display-3">A Warm Welcome!</h1>
         <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
         <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
       </header> 
 
-      <!-- Page Features -->
+      
       <div class="row text-center">
 
         <div class="col-lg-3 col-md-6 mb-4">
@@ -66,8 +93,8 @@
         </div>
 
       </div>
-      <!-- /.row -->
-
+      -->
+      
     </div>
     <!-- /.container -->
 
