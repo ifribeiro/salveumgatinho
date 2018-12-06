0ccedbdfe6e165f64332172b4a946a572b081926
jonascesar95 <jonascesar95@gmail.com>
2018-12-06 11:07:20 -0300

    confusão na home

--- application/views/home.php
+++ application/views/home.php
@@ -1,99 +1,27 @@
 
 
     <!-- Page Content -->
-    <div class="container mb-4 mt-5 clearfix">
+    <div class="container mb-4 mt-5">
 
       <!-- 3 BOTÕES -->
-      <div class="col-lg-4 float-left">
+      <div class="">
 
-        <div class="m-4 botaoRedondo border bg-info border-success rounded-circle">
+        <div class="m-3 botaoRedondo ">
           <a href="">Quero adotar</a>
         </div>
 
-        <div class="m-4 botaoRedondo border bg-info border-success rounded-circle">
-          <a href="">Quero adotar</a>
+        <div class="m-3 botaoRedondo  ">
+          <a href="">Resgate</a>
         </div>
 
-        <div class="m-4 botaoRedondo border bg-info border-success rounded-circle">
-          <a href="">Quero adotar</a>
+        <div class="m-3 botaoRedondo ">
+          <a href="">Quero ajudar</a>
         </div>
 
       </div>
 
       <!-- Texto -->
-
-      <div class="texto-apresentacao  col-lg-8 float-right">
-        <p>
-        Use os utilitários de borda para estilizar rapidamente a borda e o raio da borda de um elemento. Ótimo para imagens, botões ou qualquer outro elemento.
-        </p>
-      
-      </div>
-      
-      </div>
-      <!--
-      <header class="jumbotron my-4">
-        <h1 class="display-3">A Warm Welcome!</h1>
-        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
-        <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
-      </header> 
-
-      
-      <div class="row text-center">
-
-        <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card">
-            <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-            </div>
-            <div class="card-footer">
-              <a href="#" class="btn btn-primary">Find Out More!</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card">
-            <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
-            </div>
-            <div class="card-footer">
-              <a href="#" class="btn btn-primary">Find Out More!</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card">
-            <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-            </div>
-            <div class="card-footer">
-              <a href="#" class="btn btn-primary">Find Out More!</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card">
-            <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
-            </div>
-            <div class="card-footer">
-              <a href="#" class="btn btn-primary">Find Out More!</a>
-            </div>
-          </div>
-        </div>
-
-      </div>
-      -->
+  
       
     </div>
     <!-- /.container -->
